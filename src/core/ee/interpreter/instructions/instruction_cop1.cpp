40d629926d3040bcfc1b92f4289d27cac97163b1
strayacode <dallascuber@gmail.com>
2022-01-11 09:01:12 +1100

    remove unused files

--- src/core/ee/interpreter/instructions/instruction_cop1.cpp
+++
@@ -1,7 +0,0 @@
-#include <common/types.h>
-#include <core/ee/interpreter/interpreter.h>
-#include <core/system.h>
-
-void EEInterpreter::swc1() {
-    WriteWord(GetReg<u32>(inst.i.rs) + (s16)inst.i.imm, system->ee_cop1.GetReg(inst.i.rt));
-}
