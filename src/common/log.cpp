4b8a17c4765e66b1dc7e9558395206266233e311
strayacode <dallascuber@gmail.com>
2023-02-20 23:55:11 +1100

    add more fancy log functions

---
+++ src/common/log.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <cstdarg>
+#include <cstdlib>
+#include "common/log.h"
+
+constexpr static bool debug = true;
+
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define YELLOW "\x1B[33m"
+#define BLUE "\x1B[34m"
+#define RESET "\x1B[0m"
+
+namespace common {
+    void info(const char* format, ...) {
+        std::va_list args;
+        va_start(args, format);
+        std::printf("[" GREEN "INFO" RESET "] ");
+        std::vfprintf(stdout, format, args);
+        va_end(args);
+    }
+
+    void debug(const char* format, ...) {
+        std::va_list args;
+        va_start(args, format);
+        std::printf("[" BLUE "DEBUG" RESET "] ");
+        std::vfprintf(stdout, format, args);
+        va_end(args);
+    }
+
+    void warn(const char* format, ...) {
+        std::va_list args;
+        va_start(args, format);
+        std::printf("[" YELLOW "WARN" RESET "] ");
+        std::vfprintf(stdout, format, args);
+        va_end(args);
+    }
+
+    void error(const char* format, ...) {
+        std::va_list args;
+        va_start(args, format);
+        std::printf("[" RED "ERROR" RESET "] ");
+        std::vfprintf(stdout, format, args);
+        va_end(args);
+        std::exit(0);
+    }
+} // namespace common
--- src/common/log.h
+++ src/common/log.h
@@ -10,3 +10,10 @@
 #define log_debug(message, ...) fprintf(stdout, GREEN message "\n" CLEAR, ##__VA_ARGS__);
 #define log_warn(message, ...) fprintf(stdout, YELLOW message "\n" CLEAR, ##__VA_ARGS__);
 #define log_fatal(message, ...) fprintf(stderr, RED "fatal at " CLEAR "%s:%d " RED message "\n" CLEAR, __FILE__, __LINE__, ##__VA_ARGS__); exit(EXIT_FAILURE);
+
+namespace common {
+    void info(const char* format, ...);
+    void debug(const char* format, ...);
+    void warn(const char* format, ...);
+    void error(const char* format, ...);
+} // namespace common
